2163708254f86c0c1d0fa1667b5cfc12e01f3933
JayRaj ❤️ <87997138+JayRaj8833@users.noreply.github.com>
2023-12-31 17:26:40 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -20,7 +20,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "shareus.io",
+            "base_site": "url-shortner.com",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
